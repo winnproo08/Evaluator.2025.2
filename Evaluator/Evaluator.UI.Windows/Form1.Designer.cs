58d815378d99f59d5c295a56effcb08509f3f8ba
JORGE ANDRES ARBOLEDA ACEVEDO <winnproo@gmail.com>
2025-09-28 22:48:12 -0500

    full

--- Evaluator/Evaluator.UI.Windows/Form1.Designer.cs
+++ Evaluator/Evaluator.UI.Windows/Form1.Designer.cs
@@ -28,19 +28,330 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtDispley = new TextBox();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            btn3 = new Button();
+            btn2 = new Button();
+            btn1 = new Button();
+            btn0 = new Button();
+            btnDot = new Button();
+            btnResult = new Button();
+            btnOpenParenthesis = new Button();
+            btnMultiply = new Button();
+            btnPlus = new Button();
+            btnCloseParenthesis = new Button();
+            btnDivide = new Button();
+            btnMinus = new Button();
+            btnDelete = new Button();
+            btnClear = new Button();
+            button21 = new Button();
             SuspendLayout();
+            // 
+            // txtDispley
+            // 
+            txtDispley.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtDispley.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtDispley.Location = new Point(12, 12);
+            txtDispley.Name = "txtDispley";
+            txtDispley.Size = new Size(437, 43);
+            txtDispley.TabIndex = 0;
+            txtDispley.TextChanged += textBox1_TextChanged;
+            // 
+            // btn7
+            // 
+            btn7.BackColor = Color.White;
+            btn7.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btn7.Location = new Point(6, 66);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(76, 58);
+            btn7.TabIndex = 1;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = false;
+            btn7.Click += btn7_Click_1;
+            // 
+            // btn8
+            // 
+            btn8.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btn8.Location = new Point(98, 66);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(76, 58);
+            btn8.TabIndex = 2;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // btn9
+            // 
+            btn9.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btn9.Location = new Point(190, 66);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(76, 58);
+            btn9.TabIndex = 3;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // btn6
+            // 
+            btn6.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btn6.Location = new Point(190, 130);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(76, 58);
+            btn6.TabIndex = 6;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
+            // 
+            // btn5
+            // 
+            btn5.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btn5.Location = new Point(98, 130);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(76, 58);
+            btn5.TabIndex = 5;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
+            // 
+            // btn4
+            // 
+            btn4.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btn4.Location = new Point(6, 130);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(76, 58);
+            btn4.TabIndex = 4;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
+            // 
+            // btn3
+            // 
+            btn3.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btn3.Location = new Point(190, 194);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(76, 58);
+            btn3.TabIndex = 9;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // btn2
+            // 
+            btn2.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btn2.Location = new Point(98, 194);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(76, 58);
+            btn2.TabIndex = 8;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn1
+            // 
+            btn1.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btn1.Location = new Point(6, 194);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(76, 58);
+            btn1.TabIndex = 7;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // btn0
+            // 
+            btn0.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btn0.Location = new Point(6, 258);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(135, 65);
+            btn0.TabIndex = 10;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += btn0_Click;
+            // 
+            // btnDot
+            // 
+            btnDot.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btnDot.Location = new Point(147, 258);
+            btnDot.Name = "btnDot";
+            btnDot.Size = new Size(86, 65);
+            btnDot.TabIndex = 11;
+            btnDot.Text = ".";
+            btnDot.UseVisualStyleBackColor = true;
+            btnDot.Click += btnDot_Click;
+            // 
+            // btnResult
+            // 
+            btnResult.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btnResult.Location = new Point(239, 258);
+            btnResult.Name = "btnResult";
+            btnResult.Size = new Size(210, 65);
+            btnResult.TabIndex = 12;
+            btnResult.Text = "=";
+            btnResult.UseVisualStyleBackColor = true;
+            btnResult.Click += btnResult_Click;
+            // 
+            // btnOpenParenthesis
+            // 
+            btnOpenParenthesis.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btnOpenParenthesis.Location = new Point(272, 72);
+            btnOpenParenthesis.Name = "btnOpenParenthesis";
+            btnOpenParenthesis.Size = new Size(39, 52);
+            btnOpenParenthesis.TabIndex = 13;
+            btnOpenParenthesis.Text = "(";
+            btnOpenParenthesis.UseVisualStyleBackColor = true;
+            btnOpenParenthesis.Click += btnOpenParenthesis_Click;
+            // 
+            // btnMultiply
+            // 
+            btnMultiply.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btnMultiply.Location = new Point(272, 136);
+            btnMultiply.Name = "btnMultiply";
+            btnMultiply.Size = new Size(39, 52);
+            btnMultiply.TabIndex = 14;
+            btnMultiply.Text = "*";
+            btnMultiply.UseVisualStyleBackColor = true;
+            btnMultiply.Click += btnMultiply_Click;
+            // 
+            // btnPlus
+            // 
+            btnPlus.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btnPlus.Location = new Point(272, 200);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new Size(39, 52);
+            btnPlus.TabIndex = 15;
+            btnPlus.Text = "+";
+            btnPlus.UseVisualStyleBackColor = true;
+            btnPlus.Click += btnPlus_Click;
+            // 
+            // btnCloseParenthesis
+            // 
+            btnCloseParenthesis.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btnCloseParenthesis.Location = new Point(317, 72);
+            btnCloseParenthesis.Name = "btnCloseParenthesis";
+            btnCloseParenthesis.Size = new Size(39, 52);
+            btnCloseParenthesis.TabIndex = 16;
+            btnCloseParenthesis.Text = ")";
+            btnCloseParenthesis.UseVisualStyleBackColor = true;
+            btnCloseParenthesis.Click += btnCloseParenthesis_Click;
+            // 
+            // btnDivide
+            // 
+            btnDivide.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btnDivide.Location = new Point(317, 136);
+            btnDivide.Name = "btnDivide";
+            btnDivide.Size = new Size(39, 52);
+            btnDivide.TabIndex = 17;
+            btnDivide.Text = "/";
+            btnDivide.UseVisualStyleBackColor = true;
+            btnDivide.Click += btnDivide_Click;
+            // 
+            // btnMinus
+            // 
+            btnMinus.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btnMinus.Location = new Point(317, 200);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new Size(39, 52);
+            btnMinus.TabIndex = 18;
+            btnMinus.Text = "-";
+            btnMinus.UseVisualStyleBackColor = true;
+            btnMinus.Click += btnMinus_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btnDelete.Location = new Point(362, 69);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(100, 58);
+            btnDelete.TabIndex = 19;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            btnClear.Location = new Point(362, 133);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(100, 58);
+            btnClear.TabIndex = 20;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // button21
+            // 
+            button21.Font = new Font("Segoe UI Variable Display", 18F, FontStyle.Bold);
+            button21.Location = new Point(362, 194);
+            button21.Name = "button21";
+            button21.Size = new Size(100, 58);
+            button21.TabIndex = 21;
+            button21.Text = "^";
+            button21.UseVisualStyleBackColor = true;
+            button21.Click += button21_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.Red;
+            ClientSize = new Size(474, 341);
+            Controls.Add(button21);
+            Controls.Add(btnClear);
+            Controls.Add(btnDelete);
+            Controls.Add(btnMinus);
+            Controls.Add(btnDivide);
+            Controls.Add(btnCloseParenthesis);
+            Controls.Add(btnPlus);
+            Controls.Add(btnMultiply);
+            Controls.Add(btnOpenParenthesis);
+            Controls.Add(btnResult);
+            Controls.Add(btnDot);
+            Controls.Add(btn0);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(txtDispley);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Evaluator";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtDispley;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button btn3;
+        private Button btn2;
+        private Button btn1;
+        private Button btn0;
+        private Button btnDot;
+        private Button btnResult;
+        private Button btnOpenParenthesis;
+        private Button btnMultiply;
+        private Button btnPlus;
+        private Button btnCloseParenthesis;
+        private Button btnDivide;
+        private Button btnMinus;
+        private Button btnDelete;
+        private Button btnClear;
+        private Button button21;
     }
 }
--- Evaluator/Evaluator.UI.Windows/Form1.cs
+++ Evaluator/Evaluator.UI.Windows/Form1.cs
@@ -1,3 +1,5 @@
+using Evaluator.Core;
+
 namespace Evaluator.UI.Windows
 {
     public partial class Form1 : Form
@@ -10,6 +12,117 @@ namespace Evaluator.UI.Windows
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btn7_Click_1(object sender, EventArgs e)
+        {
+            txtDispley.Text += "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "9";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "6";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "3";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "0";
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += ".";
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "*";
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "/";
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "+";
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "-";
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text = txtDispley.Text.Substring(0, txtDispley.Text.Length - 1);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text = string.Empty;
+        }
+
+        private void btnOpenParenthesis_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "(";
+        }
+
+        private void btnCloseParenthesis_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += ")";
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += "^";
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            txtDispley.Text += $"={ExpressionEvaluator.Evaluate(txtDispley.Text)}";
         }
     }
 }
